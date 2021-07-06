72691938dd328b146cf8b9482233fbfe2b85c757
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2021-07-06 17:15:00 +0600

    1. Two Sum solved with Time complexity: n + nk

--- 1. Two Sum/index.js
+++ 1. Two Sum/index.js
@@ -5,14 +5,29 @@
  */
 var twoSum = function (nums, target) {
   const numsLen = nums.length;
+  const numsDict = {};
+  let numValue;
+  for (let i = 0; i < numsLen; i++) {
+    numValue = nums[i];
+    if (numsDict.hasOwnProperty(numValue)) {
+      numsDict[numValue].push(i);
+    } else {
+      numsDict[numValue] = [i];
+    }
+  }
+
+  let remainder, secondNumberIndex;
   for (let i = 0; i < numsLen - 1; i++) {
-    for (let j = i + 1; j < numsLen; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
+    remainder = target - nums[i];
+    if (numsDict.hasOwnProperty(remainder)) {
+      secondNumberIndex = numsDict[remainder].find(elem => elem !== i);
+      if (secondNumberIndex > -1) {
+        return [i, secondNumberIndex];
       }
     }
   }
 };
 
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 3], 6));
+// Time Complexity = n + n(1 + k) = n + nk = n(1 + k)
