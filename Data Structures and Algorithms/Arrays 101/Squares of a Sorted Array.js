ac39964234f403010b8fe361f186735e934a1952
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2021-10-09 06:32:45 +0600

    "Squares of a Sorted Array" implemented with time complexity of O(n^2)

--- Data Structures and Algorithms/Arrays 101/Squares of a Sorted Array.js
+++ Data Structures and Algorithms/Arrays 101/Squares of a Sorted Array.js
@@ -4,27 +4,24 @@
  */
 var sortedSquares = function (nums) {
     const numsLen = nums.length;
-    let currentNum;
     for (let i = 0; i < numsLen; i++) {
-        currentNum = nums[i];
-        nums[i] = currentNum ** 2;
+        nums[i] = nums[i] ** 2;
     }
 
-    let isSwapped = false
     for (let i = 0; i < numsLen - 1; i++) {
-        isSwapped = false;
-        for (let j = 0; j < numsLen - 1 - i; j++) {
-            if (nums[j] > nums[j + 1]) {
-                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-                isSwapped = true;
+        for (let j = i + 1; j < numsLen; j++) {
+            if (nums[i] > nums[j]) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
             }
-        }
-        if (!isSwapped) {
-            break;
         }
     }
 
     return nums;
 };
 
-console.log(sortedSquares([10, 9, 8, 7]));
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+
+/**
+ * Time Complexity = O(n^2)
+ * Space Complexity = O(1)
+ */
