ab3ab65d137f907b4dc4f27762d83d62d32e4ca8
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2022-01-06 20:09:02 +0600

    `1. Two Sum` with O(n) using one pash hash map

--- Problem Solutions/1. Two Sum.js
+++ Problem Solutions/1. Two Sum.js
@@ -3,23 +3,20 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-  const numsLen = nums.length,
-    mapObj = {};
-  for (let i = 0; i < numsLen; i++) {
-    mapObj[nums[i]] = i;
-  }
-
+ var twoSum = function(nums, target) {
+  const numsLen = nums.length, mapObj = {};
   let diff;
-  for (let i = 0; i < numsLen - 1; i++) {
-    diff = target - nums[i];
-    if (mapObj.hasOwnProperty(diff) && mapObj[diff] !== i) {
-      return [i, mapObj[diff]];
-    }
+  for(let i = 0; i < numsLen; i++) {
+      diff = target - nums[i];
+      if(mapObj.hasOwnProperty(diff)) {
+          return [mapObj[diff], i];
+      }
+      
+      mapObj[nums[i]] = i;
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3,3], 6));
 
 /**
  * Time Complexity = O(n)
